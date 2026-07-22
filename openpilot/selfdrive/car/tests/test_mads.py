e9fcf00f0d2dfa2605ccb231bc44358e481addd3
Isaac Barham <development@isaacbarham.com>
2026-07-22 06:57:52 -0400

    Ford: cover MADS on all CAN-FD platforms

--- openpilot/selfdrive/car/tests/test_mads.py
+++ openpilot/selfdrive/car/tests/test_mads.py
@@ -1,10 +1,24 @@
 from opendbc.car.structs import car
+from opendbc.car.ford.interface import CarInterface
+from opendbc.car.ford.values import CAR, FordFlags
 from opendbc.safety import ALTERNATIVE_EXPERIENCE
 
 from openpilot.selfdrive.car.mads import is_mads_available, is_mads_configured, is_mads_lateral_only
 
 
-def test_mads_is_available_on_ford_only():
+def test_mads_is_available_on_all_ford_canfd_platforms():
+  canfd_platforms = []
+  for candidate in CAR:
+    CP = CarInterface.get_non_essential_params(candidate)
+    if CP.flags & FordFlags.CANFD:
+      canfd_platforms.append(candidate)
+      assert is_mads_available(CP), candidate
+
+  assert CAR.FORD_F_150_LIGHTNING_MK1 in canfd_platforms
+  assert len(canfd_platforms) > 1
+
+
+def test_mads_is_unavailable_on_other_brands():
   CP = car.CarParams.new_message()
   CP.brand = "ford"
   assert is_mads_available(CP)
